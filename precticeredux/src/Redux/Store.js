665a1173c7d9e7d33d53cef49cccec35115e03ec
Abhishek Yadav <92436824+Bhi2710@users.noreply.github.com>
2023-04-15 00:02:44 +0530

    Delete precticeredux directory

--- precticeredux/src/Redux/Store.js
+++
@@ -1,7 +0,0 @@
-import { applyMiddleware, legacy_createStore } from 'redux';
-import logger  from 'redux-logger';
-import basicReducer from './Reducer';
-
-const myStore = legacy_createStore(basicReducer, applyMiddleware(logger));
-
-export default myStore;
